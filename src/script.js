b6dcc095a188a07f00e2681796f1dc627d0cc53d
fernrey <Fernando39@live.com>
2022-01-11 22:31:07 -0500

    made navbar

---
+++ src/script.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import "./style.css";
+export default function Navbar() {
+    return ( 
+        <div>
+    <nav>
+        <div className ="logo"> <h3>Zip Code Search</h3></div>
+        <ul className="bar">
+        <li>Showing Locations</li>
+        </ul>
+    </nav>
+     <div className="searchBar">Zip Code:
+    <input type="text" className="input"/>
+     </div>
+        </div> 
+    )
+}
